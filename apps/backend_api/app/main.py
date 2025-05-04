94619ffc6afbb9ed03bbba7ecf9ca37d65bc3f77
joshua <joshua92y@gmail.com>
2025-05-04 15:54:12 +0900

    독커 재수정

--- apps/backend_api/app/main.py
+++ apps/backend_api/app/main.py
@@ -6,15 +6,15 @@ import os
 import sys
 from pathlib import Path
 
-# 경로 설정
-BASE_DIR = Path(__file__).resolve().parents[3]  # joshtech
-sys.path.append(str(BASE_DIR / "apps"))  # backend_admin 찾기 위함
-sys.path.append(str(BASE_DIR / "apps" / "backend_admin"))  # config 찾기 위함
-sys.path.append(str(BASE_DIR / "packages" / "shared_schemas"))
+# 📦 BASE_DIR = 컨테이너 내에서 FastAPI 앱 루트 위치
+BASE_DIR = Path(__file__).resolve().parent
 
-# print("[DEBUG] sys.path 목록:")
-# for p in sys.path:
-#    print("  -", p)
+# ✅ Django 경로 세팅: /app/backend_admin
+sys.path.append(str(BASE_DIR / "backend_admin"))  # /app/backend_admin/config/settings.py 기준
+
+# ✅ 공통 스키마(shared_schemas) 위치 세팅: /shared_schemas
+sys.path.append("/shared_schemas")
+
 # Django 설정 연결
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend_admin.config.settings")
 django.setup()
@@ -29,15 +29,14 @@ app = FastAPI(
     version="0.1.0",
 )
 
-# ✅ 라우터 등록
+# 라우터 등록
 from .routers import contactAPI, projectAPI, resumeAPI
 
 app.include_router(resumeAPI.router)
 app.include_router(projectAPI.router)
 app.include_router(contactAPI.router)
 
-
-# 기본 루트
+# 기본 라우트
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI API!"}
