2c725028cd190ea100ff6eeaa533db6e7b323e4a
joshua <joshua92y@gmail.com>
2025-05-09 09:52:21 +0900

    ContactMessageSerializer 수정 DB 에러 픽스

--- apps/backend_admin/contact/serializers.py
+++ apps/backend_admin/contact/serializers.py
@@ -3,6 +3,9 @@ from contact.models import ContactMessage
 
 
 class ContactMessageSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(read_only=True)
+    sent_at = serializers.DateTimeField(read_only=True)
+
     class Meta:
         model = ContactMessage
         fields = [
@@ -13,4 +16,8 @@ class ContactMessageSerializer(serializers.ModelSerializer):
             "from_email",
             "to_email",
             "message_stream",
+            "created_at",
+            "sent_at",
+            "sent",
+            "failure_reason",
         ]
