941743f7e3cf396104a490f93a7f7a1a6d8dcbfe
joshua <joshua92y@gmail.com>
2025-05-19 18:01:15 +0900

    프론트 공부중 주석달기

--- apps/frontend/src/app/blog/[slug]/page.tsx
+++ apps/frontend/src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+//src/app/blog/[slug]/page.tsx
 import { notFound } from "next/navigation";
 import { CustomMDX } from "@/components/mdx";
 import { getPosts } from "@/app/utils/utils";
@@ -17,36 +18,33 @@ import ScrollToHash from "@/components/ScrollToHash";
 import { Metadata } from "next";
 import { Meta, Schema } from "@/once-ui/modules";
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
-  const posts = getPosts(["src", "app", "blog", "posts"]);
-  return posts.map((post) => ({
-    slug: post.slug,
+//generateStaticParams()에 getPosts의 슬러그 배열을 주고 스태틱 파라미터를 미리 생성
+export async function generateStaticParams(): Promise<{ slug: string }[]> { //Promise<{ slug: string }[]> 타입 제네릭 타입 파라미터
+  const posts = getPosts(["src", "app", "blog", "posts"]); //getPosts에서 받아온 객체에서 slug를 추출하여 posts 배열에 할당
+  return posts.map((post) => ({ //posts에서 받아온 객체에서 slug를 추출하여 post 배열에 할당
+    slug: post.slug, 
   }));
 }
+//사용자가 특정 포스트를 방문할 때 메타데이터를 생성 or export biled시에 메타데이터를 생성
+export async function generateMetadata({params,}: {params: Promise<{ slug: string | string[] }>;}): Promise<Metadata> {
+  const routeParams = await params; // Next.js App Router에서 동적 경로(/blog/[slug])의 값(slug)을 params라는 객체로 제공하고,이 비동기 파라미터를 await로 풀어서 routeParams에 할당한다.
+  const slugPath = Array.isArray(routeParams.slug) // 경로가 배열인지 확인하고 slugPath에 URL 경로를 할당
+    ? routeParams.slug.join("/") // 배열이면 배열의 요소를 결합 routeParams.slug = ["foo", "bar"] => ["foo/bar"]
+    : routeParams.slug || ""; // 배열이 아니면 경로를 반환 routeParams.slug = ["foo"] => ["foo"]
 
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ slug: string | string[] }>;
-}): Promise<Metadata> {
-  const routeParams = await params;
-  const slugPath = Array.isArray(routeParams.slug)
-    ? routeParams.slug.join("/")
-    : routeParams.slug || "";
-
-  const posts = getPosts(["src", "app", "blog", "posts"]);
-  let post = posts.find((post) => post.slug === slugPath);
+  const posts = getPosts(["src", "app", "blog", "posts"]); 
+  let post = posts.find((post) => post.slug === slugPath); // 슬러그 벨류가 slugPath와 일치하는 포스트 객체를 반환
 
   if (!post) return {};
 
   return Meta.generate({
-    title: post.metadata.title,
-    description: post.metadata.summary,
-    baseURL: baseURL,
-    image: post.metadata.image
-      ? `${baseURL}${post.metadata.image}`
-      : `${baseURL}/og?title=${post.metadata.title}`,
-    path: `${blog.path}/${post.slug}`,
+    title: post.metadata.title, // 포스트 객체의 메타데이터에서 title을 가져옴
+    description: post.metadata.summary, // 포스트 객체의 메타데이터에서 summary를 가져옴
+    baseURL: baseURL, // 베이스 URL을 가져옴
+    image: post.metadata.image // 포스트 객체의 메타데이터에서 image를 가져옴
+      ? `${baseURL}${post.metadata.image}` // 이미지가 있으면 이미지 경로를 가져옴
+      : `${baseURL}/og?title=${post.metadata.title}`, // 이미지가 없으면 타이틀을 가져오고 og로 이미지 생성
+    path: `${blog.path}/${post.slug}`, // import한 blog의 객체(resources/index.ts<=content.js)의 path와 포스트 슬러그를 결합하여 포스트의 고유 경로를 생성
   });
 }
 
--- apps/frontend/src/app/utils/utils.ts
+++ apps/frontend/src/app/utils/utils.ts
@@ -1,8 +1,9 @@
 //src/app/utils/utils.ts
 import fs from "fs";
 import path from "path";
-import matter from "gray-matter";
+import matter from "gray-matter"; //gray-matter는 마크다운 파일에서 --- 구분자로 시작하는 YAML 블록을 파싱
 
+// 팀 타입 정의
 type Team = {
   name: string;
   role: string;
@@ -10,7 +11,8 @@ type Team = {
   linkedIn: string;
 };
 
-type Metadata = {
+// 메타데이터는 포스트의 정보를 담고 있는 객체 타입 정의
+type Metadata = { 
   title: string;
   publishedAt: string;
   summary: string;
@@ -23,41 +25,42 @@ type Metadata = {
 
 import { notFound } from "next/navigation";
 
+//getMDXData에서 호출되고 postsdir경로(/project-root/src/app/blog/posts)를 받아와서 디렉토리의 모든 파일을 읽고 .mdx로 끝나는 파일명을 반환
 function getMDXFiles(dir: string) {
-  if (!fs.existsSync(dir)) {
-    notFound();
+  if (!fs.existsSync(dir)) { //fs.existsSync(dir) 디렉토리가 존재하는지 확인_boolean_node.js함수
+    notFound(); // 디렉토리가 없으면 404 페이지로 리다이렉트
   }
-
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+  
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx"); // 예시 결과: ["seo.mdx", "work.mdx", "blog.mdx".....]
 }
-
+//getMDXFiles에서 호출되고 postsdir경로와 파일명을 받아와서 파일을 읽고 메타데이터와 콘텐츠를 반환
 function readMDXFile(filePath: string) {
-  if (!fs.existsSync(filePath)) {
-    notFound();
+  if (!fs.existsSync(filePath)) { //fs.existsSync(filePath) 파일이 존재하는지 확인_boolean_node.js함수
+    notFound(); // 파일이 없으면 404 페이지로 리다이렉트
   }
 
-  const rawContent = fs.readFileSync(filePath, "utf-8");
-  const { data, content } = matter(rawContent);
+  const rawContent = fs.readFileSync(filePath, "utf-8"); //fs.readFileSync(filePath, "utf-8") 파일을 읽고 문자열로 반환
+  const { data, content } = matter(rawContent); // --- 구분자로 데이터와 콘텐츠를 분리
 
   const metadata: Metadata = {
-    title: data.title || "",
-    publishedAt: data.publishedAt,
-    summary: data.summary || "",
-    image: data.image || "",
-    images: data.images || [],
-    tag: data.tag || [],
-    team: data.team || [],
-    link: data.link || "",
+    title: data.title || "", // 데이터에서 타이틀을 가져오고 없으면 빈 문자열로 초기화
+    publishedAt: data.publishedAt, // 데이터에서 게시일을 가져오고 없으면 빈 문자열로 초기화
+    summary: data.summary || "", // 데이터에서 요약을 가져오고 없으면 빈 문자열로 초기화
+    image: data.image || "", // 데이터에서 이미지를 가져오고 없으면 빈 문자열로 초기화
+    images: data.images || [], // 데이터에서 이미지 배열을 가져오고 없으면 빈 배열로 초기화
+    tag: data.tag || [], // 데이터에서 태그를 가져오고 없으면 빈 배열로 초기화
+    team: data.team || [], // 데이터에서 팀을 가져오고 없으면 빈 배열로 초기화
+    link: data.link || "", // 데이터에서 링크를 가져오고 없으면 빈 문자열로 초기화
   };
 
-  return { metadata, content };
+  return { metadata, content }; // 메타데이터와 콘텐츠를 반환
 }
-
+//getPosts에서 호출되고 postsdir경로를 받아와서 getMDXFiles로 전달 후 변환된 배열을 맵핑(메타데이터, 콘텐츠)=>readMDXFile 슬러그=> 파일명
 function getMDXData(dir: string) {
-  const mdxFiles = getMDXFiles(dir);
-  return mdxFiles.map((file) => {
-    const { metadata, content } = readMDXFile(path.join(dir, file));
-    const slug = path.basename(file, path.extname(file));
+  const mdxFiles = getMDXFiles(dir); // 예시 결과: ["seo.mdx", "work.mdx", "blog.mdx".....]
+  return mdxFiles.map((file) => { // 예시 결과: [{metadata, slug, content}, {metadata, slug, content}, {metadata, slug, content}.....]
+    const { metadata, content } = readMDXFile(path.join(dir, file)); // 파일 경로를 만들고 readMDXFile로 전달 후 메타데이터와 콘텐츠를 반환
+    const slug = path.basename(file, path.extname(file)); // basename로 전체 파일 이름과 path.extname(file)로 파일 확장자를 넣어서 제거한 파일이름을 슬러그로 변환
 
     return {
       metadata,
@@ -67,7 +70,9 @@ function getMDXData(dir: string) {
   });
 }
 
+//페이지에서 getPosts호출 postsdir경로 배열을 받아와서 디렉토리 경로를 만들고 getMDXData로 전달
 export function getPosts(customPath = ["", "", "", ""]) {
   const postsDir = path.join(process.cwd(), ...customPath);
-  return getMDXData(postsDir);
+  // 예시 결과: /project-root/src/app/blog/posts
+  return getMDXData(postsDir); // 예시 결과: 맵핑된 배열  [{metadata: { title: "SEO 글", ... },, slug: "seo",content: "# SEO 글 내용"}, {metadata, slug, content}, {metadata, slug, content}.....]
 }
