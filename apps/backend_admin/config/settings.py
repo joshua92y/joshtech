d62b3adaea2cd5dc5376247ac82b20688b466749
joshua <joshua92y@gmail.com>
2025-05-22 10:16:38 +0900

    도커에 shared_queue 추가

--- apps/backend_admin/config/settings.py
+++ apps/backend_admin/config/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # 🔐 보안 설정
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1").split(",")
 
 # 🌐 CORS/CSRF 설정
 CORS_ALLOW_CREDENTIALS = True
